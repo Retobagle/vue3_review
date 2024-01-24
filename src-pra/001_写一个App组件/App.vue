f832380194d55e3138dd9af56245a81e7c35e21e
Retobagle <Retobagle@163.com>
2024-01-24 17:59:56 +0800

    feat: setup

---
+++ src-pra/001_写一个App组件/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1 class="title">来复习了</h1>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .title {
+    background-color: #ddd;
+    padding: 20px;
+    border-radius: 10px;
+    box-shadow: 0 0 10px;
+  }
+</style>
