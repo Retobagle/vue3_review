a37bd20962ad2e567017c2c582dff128d5a1e8f1
huangweixing <retobagle@163.com>
2024-04-02 22:21:55 +0800

    feat: 路由重定向

---
+++ src-pra/029_路由_重定向/pages/News.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="news">
+    <!-- 导航区 -->
+    <ul>
+      <li v-for="news in newList" :key="news.id">
+        <button @click="shwoNewsDetail(news)">查看新闻</button>
+        <RouterLink
+          :to="{
+            name:'xiangqing',
+            query:{
+              id:news.id,
+              title:news.title,
+              content:news.content,
+            }
+          }"
+        >
+          {{news.title}}
+        </RouterLink>
+      </li>
+    </ul>
+    <!-- 展示区 -->
+    <div class="news-content">
+      <RouterView></RouterView>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="News">
+  import {reactive} from 'vue'
+  import {useRouter} from 'vue-router'
+
+  const newList = reactive([
+    {id:'adjklajd01',title:'不错01',content:'升职加薪01'},
+    {id:'adjklajd02',title:'不错02',content:'升职加薪02'},
+    {id:'adjklajd03',title:'不错03',content:'升职加薪03'},
+    {id:'adjklajd04',title:'不错04',content:'升职加薪04'},
+  ])
+
+  const router = useRouter()
+  interface NewsInter {
+    id:string,
+    title:string,
+    content:string
+  }
+  function shwoNewsDetail(news:NewsInter){
+    router.replace({
+      name:'xiangqing',
+      query:{
+        id:news.id,
+        title:news.title,
+        content:news.content,
+      }
+    })
+  }
+</script>
+
+<style scoped>
+/* 新闻 */
+.news {
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+  height: 100%;
+}
+.news ul {
+  margin-top: 30px;
+  list-style: none;
+  padding-left: 10px;
+}
+.news li>a {
+  font-size: 18px;
+  line-height: 40px;
+  text-decoration: none;
+  color: #64967E;
+  text-shadow: 0 0 1px rgb(0, 84, 0);
+}
+.news-content {
+  width: 70%;
+  height: 90%;
+  border: 1px solid;
+  margin-top: 20px;
+  border-radius: 10px;
+}
+</style>
