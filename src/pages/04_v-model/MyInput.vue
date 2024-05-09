aba197f3195bb49ef5a471cd7a400964c3bb075b
huangweixing <retobagle@163.com>
2024-05-09 19:17:04 +0800

    feat: 组件通信；其他api；新内置组件

---
+++ src/pages/04_v-model/MyInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input 
+    type="text" 
+    :value="name"
+    @input="emit('update:name',(<HTMLInputElement>$event.target).value)"
+  >
+  <br>
+  <input 
+    type="text" 
+    :value="psd"
+    @input="emit('update:psd',(<HTMLInputElement>$event.target).value)"
+  >
+  <br>
+</template>
+
+<script setup lang="ts" name="MyInput">
+  defineProps(['name','psd'])
+  const emit = defineEmits(['update:name', 'update:psd'])
+</script>
+
+<style scoped>
+  input {
+    border: 2px solid black;
+    background-image: linear-gradient(45deg,red,yellow,green);
+    height: 30px;
+    font-size: 20px;
+    color: white;
+  }
+</style>
